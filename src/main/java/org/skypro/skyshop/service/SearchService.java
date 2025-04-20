990eabfccd9e1396fee1f66659e42c2574354496
79774 <Cinnabon_09@.com>
2025-04-20 20:48:21 +0300

    .

--- src/main/java/org/skypro/skyshop/service/SearchService.java
+++ src/main/java/org/skypro/skyshop/service/SearchService.java
@@ -1,9 +1,5 @@
-package org.skypro.skyshop.model.search;
-
-
+package org.skypro.skyshop.service;
 import org.skypro.skyshop.model.search.SearchResult;
-import org.skypro.skyshop.model.search.Searchable;
-import org.skypro.skyshop.service.StorageService;
 import org.springframework.stereotype.Service;
 import java.util.Collection;
 import java.util.stream.Collectors;
--- src/main/java/org/skypro/skyshop/service/StorageService.java
+++ src/main/java/org/skypro/skyshop/service/StorageService.java
@@ -1,6 +1,5 @@
 package org.skypro.skyshop.service;
 
-
 import org.skypro.skyshop.model.article.Article;
 import org.skypro.skyshop.model.product.Book;
 import org.skypro.skyshop.model.product.Product;
@@ -50,4 +49,10 @@ public class StorageService {
         all.addAll(products.values());
         return all;
     }
+
+
+    public Optional<Product> getProductById(UUID id) {
+        return Optional.ofNullable(products.get(id)); // Было: availableProducts
+    }
+
 }
